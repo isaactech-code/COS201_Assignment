6b6c3e06706e7d04745cabe597a8d4b109f4d0e3
Isaac <111354561+isaactech-code@users.noreply.github.com>
2025-01-27 07:24:56 +0100

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,25 @@
+public class Q2 {
+    public static void main(String[] args) {
+        System.out.println("USA's Flag (Single Loop):");
+        for (int i = 1; i<=6; i++){
+            if (i<=3){
+                System.out.println("* * * * = = = = = = =");
+            } else {
+                System.out.println("= = = = = = = = = = =");
+            }
+        }
+        System.out.println();
+
+        System.out.println("USA Flag (Nested Loop):");
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 12; col++) {
+                if (row < 3 && col < 4) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("= ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
